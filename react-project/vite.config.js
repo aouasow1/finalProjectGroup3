e2558a6b3c5c41bad01b1234b7ea9baf777737b6
Aoua <aoua.sow1@gmail.com>
2025-04-21 01:17:10 -0400

    update vite content

--- react-project/vite.config.js
+++ react-project/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/finalProjectGoup3/", plugins: [react()],
+  base: "/finalProjectGroup3/", plugins: [react()],
 })
