5419a21ccebb11b756bd4d57e3096b212222f168
Aoua <aoua.sow1@gmail.com>
2025-05-02 13:20:44 -0400

    Update

--- react-project/src/components/pages/course.jsx
+++ react-project/src/components/pages/course.jsx
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from 'react';
-import './course.css'
+
 
 export const Courses = () => {
     const [content, setContent] = useState(<AllCourse showForm={showForm}/>);
